88ce5a5d9e55c5c256d6215bb8d96656e5ba65f5
Luis Lavieri <lumalav@gmail.com>
2016-01-11 23:55:59 -0500

    Initial Commit

---
+++ 3DCytoFlow/src/3DCytoFlow/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _3DCytoFlow.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
---
+++ 3DCytoFlow/src/3DCytoFlow/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _3DCytoFlow.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
