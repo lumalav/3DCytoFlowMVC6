ba1c01022d557bd6469ec3f84cb2a9669e59db68
Luis Lavieri <lumalav@gmail.com>
2016-01-29 00:20:13 -0500

    Fixed menu

--- 3DCytoFlow/src/3DCytoFlow/Models/ApplicationDbContext.cs
+++ 3DCytoFlow/src/3DCytoFlow/Models/ApplicationDbContext.cs
@@ -1,23 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Data.Entity;
-using _3DCytoFlow;
 
 namespace _3DCytoFlow.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
-        }
-
         public DbSet<Analysis> Analyses { get; set; }
         public DbSet<Cluster> Clusters { get; set; }
         public DbSet<Patient> Patients { get; set; }
